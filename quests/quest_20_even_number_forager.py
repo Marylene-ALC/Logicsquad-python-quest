5830b195d3256504578a73ce8c18f9adc2ce3bd6
Abduljabar Abdulkadir <a.abdulkadi@alustudent.com>
2026-04-16 02:07:29 -0700

    quest_20_even_number_forager.py added

---
+++ quests/quest_20_even_number_forager.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for number in range(1, 21):
+    if number % 2 == 0:
+        print("Even numbers:", number)
+    elif number % 2 == 1:
+        print("Odd numbers", number)
+    else:
+        print("No numbers")
