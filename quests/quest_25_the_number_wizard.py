aeb92e86d54fcd7b6bc0fe892e0c317859b0036f
Elizabeth-Ater <e.ater@alustudent.com>
2026-04-16 13:45:23 +0400

    Quest 25

---
+++ quests/quest_25_the_number_wizard.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+secret_number = 42
+guess = int(input("Please enter a number"))
+
+if guess > secret_number:
+    print("Number too long")
+elif guess < secret_number:
+    print("Number too short")
+else:
+    print("Correct number!")
