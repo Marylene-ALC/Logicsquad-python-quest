6726bbfb3b2f9a9d9145a0c8cd3f3899674b893f
adac-21 <c.mmadu@alustudent.com>
2026-04-16 12:04:05 +0400

    Calculated the total cost of 3 dragon scales (10 gold) and 5 elf tears (3 gold) using basic arithmetic operators

---
+++ quests/quest_08_the_portion_brewer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3 dragon scales (10 gold each) and 5 elf tears (3 gold each)
+total_dragon_scales = 3*10
+total_elf_tears = 5*3
+
+#Calculating the total cost.
+total_cost = total_dragon_scales + total_elf_tears
+print (f"total cost is {total_cost}") 
